10504d7049b1ab4bcb38f809f534a0ee636845fc
tomomih217 <ku2gelschrei21ber7@gmail.com>
2023-03-28 01:21:28 +0900

    Add: events with follow user in event_calender page to be able to check events follow user attended

--- app/controllers/mypage/event_calendars_controller.rb
+++ app/controllers/mypage/event_calendars_controller.rb
@@ -5,5 +5,6 @@ class Mypage::EventCalendarsController < Mypage::BaseController
     @bookmark_events = current_user.bookmark_events
     @future_events = current_user.attend_events.future
     @past_events = current_user.attend_events.past
+    @follow_events = Event.joins(:attendees).where("event_attendances.user_id IN (?) OR events.user_id = ?", current_user.following_ids, current_user.id)
   end
 end
